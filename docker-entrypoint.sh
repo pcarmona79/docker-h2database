aea2f481e096a233c2a5b8890398556642a79ae2
Pablo Carmona A <pcarmona@sictel.cl>
2023-03-08 16:28:33 -0300

    Updates docker-entrypoint.sh to use the new enviroment variables.
    Also, the content of directory /docker-entrypoint-initdb.d is readed in
    alphabetic order, so scripts can use numbers to designate a
    specific order to be executed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
 SERVER="java -cp /h2/bin/h2.jar org.h2.tools.Server"
 RUNSCRIPT="java -cp /h2/bin/h2.jar org.h2.tools.RunScript"
 
 runSql() {
-  filename=$(basename "$1")
-  db="${filename%.*}"
-  url="jdbc:h2:$H2DATA/$db"
+  if [[ ! -z "$H2_OPTIONS" ]]; then
+    url="jdbc:h2:$H2DATA/$H2_DBNAME;$H2_OPTIONS";
+  else
+    url="jdbc:h2:$H2DATA/$H2_DBNAME";
+  fi
   echo "using url $url"
-  $RUNSCRIPT -script "$1" -url "$url"
+  $RUNSCRIPT -script "$1" -url "$url" -user "$H2_USER" -password "$H2_PASSWORD"
 }
 
 mkdir -p "$H2DATA"
@@ -17,12 +19,14 @@ mkdir -p "$H2DATA"
 if [ ! -f "$H2DATA/.initdb.completed" ]; then
 
   echo
-  for f in /docker-entrypoint-initdb.d/*; do
-    case "$f" in
-      *.sh)     echo "$0: running $f"; . "$f" ;;
-      *.sql)    echo "$0: running $f"; runSql "$f" ;;
-      *)        echo "$0: ignoring $f" ;;
-    esac
+  for f in $( ls -d /docker-entrypoint-initdb.d/* | sort -n ); do
+    if [[ ! -d "$f" ]]; then
+      case "$f" in
+        *.sh)     echo "$0: running $f"; . "$f" ;;
+        *.sql)    echo "$0: running $f"; runSql "$f" ;;
+        *)        echo "$0: ignoring $f" ;;
+      esac
+    fi
     echo
   done
   touch "$H2DATA/.initdb.completed"
